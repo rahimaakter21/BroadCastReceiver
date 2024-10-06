863c8a2affc70ca7ca10b9805364e36ae741fe41
rahimaakter21 <rahimaakter20024@gmail.com>
2024-10-06 10:52:50 +0600

    broad cast- air plane mode change receiver

---
+++ app/src/main/java/com/example/broadcastreceiver/AirPlaneModeChangedReceiver.kt
@@ -0,0 +1,17 @@
+package com.example.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AirPlaneModeChangedReceiver:BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+       if(intent?.action == Intent.ACTION_AIRPLANE_MODE_CHANGED){
+           Toast.makeText(context,"Airplane Mode Changed",Toast.LENGTH_SHORT).show()
+
+       }
+
+    }
+}
